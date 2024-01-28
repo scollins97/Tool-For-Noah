4eca93c60b6b0657eacfc6ecf9c6e35b799ed6a4
seancollins97 <srcollins575@gmail.com>
2024-01-28 17:57:36 -0500

    started main process

--- main.py
+++ main.py
@@ -16,4 +16,29 @@ this method is going to take a list of "combinations" and sort their final score
 the scores that meet the users goal (or get the closest compared to the rest) should appear at top of list
 sorting by list
 '''
-def sortMyCombos():
+#def sortMyCombos(theCombinations):
+    #some future code
+
+#the main thing
+    
+#this is the goal we're trying to reach    
+goal = input("enter the amount you need in the work orders: ")
+#a list to store the numbers of the different lots
+numbers = []
+#asking to user to enter in all the different lot amounts
+print("type in each amount of your lots followed by 'enter'. \nenter 0 when you're done" )
+#this boolean statement will be turned false when the user enters a 0, ending the while loop
+gatheringLots = True
+#because there are any given number of different lots, a while statement seemed appropriate
+while gatheringLots == True:
+    #placeholder to save users input to later insert into the list
+    nextNumber = input()
+    #had to use int method to force input be seen as an integer
+    if int(nextNumber) <= 0:
+        gatheringLots = False
+    else: 
+        numbers.append(int(nextNumber))
+
+#print out the list to make sure it works properly
+print(numbers)
+print("You goal is " + goal + ".")
